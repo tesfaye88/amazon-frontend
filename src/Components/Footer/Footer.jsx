a093b14a00308c70ac896708ac6bcb5a0b18a85b
tesfaye88 <123720165+tesfaye88@users.noreply.github.com>
2024-12-11 00:26:00 -0800

    update footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,11 +15,16 @@ const Footer = () => {
         <div className={classes.footer_links}>
           <h3 className={classes.footer_linksTitle}>Get to Know Us</h3>
           <Link  to="/">
-          <p className={classes.footer_link}>About Us</p>
-          <p className={classes.footer_link}>Careers</p>
-          <p className={classes.footer_link}>Press Releases</p>
-          <p className={classes.footer_link}>Amazon Cares</p>
-          <p className={classes.footer_link}>Gift a Smile</p>
+          <div className={classes.footer_link}>
+          <ul  >
+          <li>About Us</li>
+          <li>Careers</li>
+          <li>Press Releases</li>
+          <li>Amazon Cares</li>
+          <li >Gift a Smile</li>
+          </ul>
+          </div>
+          
         
           </Link>
           
@@ -27,32 +32,45 @@ const Footer = () => {
         <div className={classes.footer_links}>
           <h3 className={classes.footer_linksTitle}>Connect with Us</h3>
           <Link to='/'>
-          <p className={classes.footer_link}>Facebook</p>
-          <p className={classes.footer_link}>Twitter</p>
-          <p className={classes.footer_link}>Instagram</p>
+         <div className={classes.footer_link}>
+         <ul >
+         <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+         </ul>
+         </div>
           </Link>
         </div>
                   
         <div className={classes.footer_links}>
           <h3 className={classes.footer_linksTitle}>Make Money with Us</h3>
           <Link to='/'>
-          <p className={classes.footer_link}>Sell on Amazon</p>
-          <p className={classes.footer_link}>Sell under Amazon Accelerator</p>
-          <p className={classes.footer_link}>Amazon Global Selling</p>
-          <p className={classes.footer_link}>Become an Affiliate</p>
-          <p className={classes.footer_link}>Amazon Pay on Merchants</p>
-          <p className={classes.footer_link}>Advertise Your Products</p>
+          <div className={classes.footer_link}>
+          <ul >
+          <li>Sell on Amazon</li>
+          <li>Sell under Amazon Accelerator</li>
+          <li >Amazon Global Selling</li>
+          <li>Become an Affiliate</li>
+          <li >Amazon Pay on Merchants</li>
+          <li >Advertise Your Products</li>
+          </ul>
+          </div>
           </Link>
         </div>
         <div className={classes.footer_links}>
           <h3 className={classes.footer_linksTitle}>Let Us Help You</h3>
          <Link to='/'>
-         <p className={classes.footer_link}>COVID-19 and Amazon</p>
-          <p className={classes.footer_link}>Your Account</p>
-          <p className={classes.footer_link}>Returns Centre</p>
-          <p className={classes.footer_link}>100% Purchase Protection</p>
-          <p className={classes.footer_link}>Amazon App Download</p>
-          <p className={classes.footer_link}>Help</p>
+         <div className={classes.footer_link}>
+         <ul  >
+         <li >COVID-19 and Amazon</li>
+          <li >Your Account</li>
+          <li>Returns Centre</li>
+          <li>100% Purchase Protection</li>
+          <li>Amazon App Download</li>
+          <li>Help</li>
+         </ul>
+         </div>
+                  
          </Link>
         </div>
       </div>
